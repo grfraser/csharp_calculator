d7d6c14a0cedc69e7128b181619b4b53c9f433c2
Grant <grantrf93@utexas.edu>
2019-06-05 22:34:44 -0500

    C# Calculator Program
    Not as user friendly. Need to figure out a way to create a better UI, preferably on a webpage. Trying to learn ASP.NET framework with Visual Studio.

---
+++ cs_calc/cs_calc/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace cs_calc
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.Write("Enter num1: ");
+            double num1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Enter operator: ");
+            string op = Console.ReadLine();
+
+            Console.Write("Enter num2: ");
+            double num2 = Convert.ToDouble(Console.ReadLine());
+
+            if (op == "+")
+            {
+                Console.WriteLine(num1 + num2);
+            }
+            else if (op == "-")
+            {
+                Console.WriteLine(num1 - num2);
+            }
+            else if (op == "/")
+            {
+                Console.WriteLine(num1 / num2);
+            }
+            else if (op == "*")
+            {
+                Console.WriteLine(num1 * num2);
+            }
+            else
+            {
+                Console.WriteLine("Invalid Operator");
+            }
+
+        }
+    }
+}
